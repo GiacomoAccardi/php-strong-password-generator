6fba7400f25179893fc65b5dd9b406425676ced0
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-09-06 17:24:34 +0200

    [FEAT] My script can now generate a password

--- index.php
+++ index.php
@@ -1,16 +1,34 @@
 <?php
 
-    $passwordLength = $_GET['passLength'];
-    //var_dump($passwordLength);
-
+    
+    $passwordLength = intval($_GET['passLength']);
+    var_dump($passwordLength);
     //creo variabile contentente tutti i caratteri utilizzabili
     $charList = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!?~@#-_+<>[]{}';
 
-    //creo una variabile per contenere la nuova password
-    $generatedPass = '';
+    function generatePassword($passwordLength, $charList){
+        //creo una variabile per contenere la nuova password
+        $password = '';   
+        $charListLength = strlen($charList);
 
-    
+        // Genera una password lunga quanto specificato
+        for ($i = 0; $i < $passwordLength; $i++) {
+            // Seleziona un indice casuale dalla lista dei caratteri
+            $randomIndex = random_int(0, $charListLength - 1);
+            // Aggiunge il carattere corrispondente alla password
+            $password .= $charList[$randomIndex];
+        }
+        return $password;
+    };
 
+    // Controlla che la lunghezza sia valida
+    if (isset($passwordLength) && $passwordLength > 3 && $passwordLength < 18) {
+        $generatedPassword = generatePassword($passwordLength, $charList);
+        //echo "Password generata: " . $generatedPassword;
+        var_dump($generatedPassword);
+    } else {
+        echo "Inserisci una lunghezza valida.";
+    }
 ?>
 
 <!DOCTYPE html>
