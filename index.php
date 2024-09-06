ec46ec08f479a9aaea844970289dd063e17b1667
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-09-06 15:37:14 +0200

    [UPD] Created css file

--- index.php
+++ index.php
@@ -9,10 +9,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BoolPassGen</title>
+    <!-- importo Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 </head>
 
 <body>
-    
+    <header>
+        <nav class="navbar bg-body-secondary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                <img src="https://d92mrp7hetgfk.cloudfront.net/images/sites/misc/Boolean/original.png?1623187562" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">
+                BoolPassGen
+                </a>
+            </div>
+        </nav>
+    </header>
+
+    <main class="bg-secondary-subtle">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    
+                </div>
+            </div>
+        </div>
+    </main>
 </body>
 
 </html>
